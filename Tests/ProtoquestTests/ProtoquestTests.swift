14926cf1102db11d82c9b22c6530aa8a653fac10
Julian Dunskus <julian.dunskus@gmail.com>
2021-05-05 23:37:04 +0200

    full-stack tests

--- Tests/ProtoquestTests/ProtoquestTests.swift
+++ Tests/ProtoquestTests/ProtoquestTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import Protoquest
+import Combine
 
 final class ProtoquestTests: XCTestCase {
 	let client = TestClient()
@@ -24,10 +25,46 @@ final class ProtoquestTests: XCTestCase {
 		let rawWithout = try client.rawRequest(for: without)
 		XCTAssertEqual(rawWithout.url!.absoluteString, "https://test.com/url_parameters/query_items?one=test")
 	}
+	
+	func testSending() {
+		verifyResponse(for: TestGetStringRequest()) { response in
+			XCTAssertEqual(response, "echo ")
+		}
+		
+		verifyResponse(for: TestJSONStringRequest(body: .init(text: "hello!"))) { response in
+			XCTAssertEqual(response, #"echo {"text":"hello!"}"#)
+		}
+	}
+	
+	func verifyResponse<R: Request>(for request: R, verify: @escaping (R.Response) -> Void) {
+		let responseExpectation = expectation(description: "response")
+		let cancellable = client.send(request).sink {
+			switch $0 {
+			case .finished:
+				break
+			case .failure(let error):
+				XCTFail("error executing request: \(error)")
+			}
+		} receiveValue: {
+			verify($0)
+			responseExpectation.fulfill()
+		}
+		
+		waitForExpectations(timeout: 0.1, handler: nil)
+		_ = cancellable
+	}
 }
 
 struct TestClient: Protoclient {
 	let baseURL = URL(string: "https://test.com")!
+	
+	let echo = "echo ".data(using: .utf8)!
+	
+	func dispatch<R>(_ rawRequest: URLRequest, for request: R) -> AnyPublisher<DataTaskResult, Error> where R : Request {
+		Just((data: echo + (rawRequest.httpBody ?? Data()), response: URLResponse()))
+			.setFailureType(to: Error.self)
+			.eraseToAnyPublisher()
+	}
 }
 
 struct TestRequest: JSONJSONRequest, Encodable {
@@ -40,7 +77,7 @@ struct TestRequest: JSONJSONRequest, Encodable {
 	}
 }
 
-struct TestURLParameterRequest: GetRequest, StringDecodingRequest, Encodable {
+struct TestURLParameterRequest: GetRequest, StringDecodingRequest {
 	var path: String { "url_parameters/query_items" }
 	
 	var param1: String
@@ -53,4 +90,14 @@ struct TestURLParameterRequest: GetRequest, StringDecodingRequest, Encodable {
 			("two", param2)
 		}
 	}
+}
+
+struct TestGetStringRequest: GetRequest, StringDecodingRequest {}
+
+struct TestJSONStringRequest: JSONEncodingRequest, StringDecodingRequest {
+	struct Body: Encodable {
+		var text: String
+	}
+	
+	var body: Body
 }
