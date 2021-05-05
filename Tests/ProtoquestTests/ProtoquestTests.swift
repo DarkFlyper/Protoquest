c558c0cf7b558e3277f42e3e60c1e1a11426c5e5
Julian Dunskus <julian.dunskus@gmail.com>
2021-05-05 22:45:27 +0200

    more tests

--- Tests/ProtoquestTests/ProtoquestTests.swift
+++ Tests/ProtoquestTests/ProtoquestTests.swift
@@ -6,11 +6,19 @@ final class ProtoquestTests: XCTestCase {
 	
 	func testExample() throws {
 		let raw = try client.rawRequest(for: TestRequest(text: "hi!"))
-		let json = String(bytes: raw.httpBody!, encoding: .utf8)!
 		XCTAssertEqual(raw.url!.absoluteString, "https://test.com/example/path")
 		XCTAssertEqual(raw.httpMethod, "POST")
 		XCTAssertEqual(raw.value(forHTTPHeaderField: "Content-Type"), "application/json")
+		let json = String(bytes: raw.httpBody!, encoding: .utf8)!
 		XCTAssertEqual(json, #"{"text":"hi!"}"#)
+	}
+	
+	func testURLParams() throws {
+		let raw = try client.rawRequest(for: TestURLParameterRequest(param1: "te&st"))
+		XCTAssertEqual(raw.url!.absoluteString, "https://test.com/url_parameters/query_items?one=te%26st&two=42")
+		XCTAssertEqual(raw.httpMethod, "GET")
+		XCTAssertNil(raw.value(forHTTPHeaderField: "Content-Type"))
+		XCTAssertNil(raw.httpBody)
 	}
 }
 
@@ -24,6 +32,21 @@ struct TestRequest: JSONJSONRequest, Encodable {
 	var text: String
 	
 	struct Response: Decodable {
-		var text: String
+		var value: Int
+	}
+}
+
+struct TestURLParameterRequest: GetRequest, StringDecodingRequest, Encodable {
+	var path: String { "url_parameters/query_items" }
+	
+	var param1: String
+	var param2 = 42
+	var shouldIncludeSecondParam = true
+	
+	var urlParams: [URLParameter] {
+		("one", param1)
+		if shouldIncludeSecondParam {
+			("two", param2)
+		}
 	}
 }
